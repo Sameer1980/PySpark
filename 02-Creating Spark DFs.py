55d5cd0ca46b9f5da7836112823ed0771453cfff
Samir Banerjee <smrbanerjee43@gmail.com>
2026-05-26 09:53:50 +0530

    Implement various CSV reading options for Spark DataFrames
    Added multiple methods to read CSV files into DataFrames with Spark.

--- 02-Creating Spark DFs.py
+++ 02-Creating Spark DFs.py
@@ -6,3 +6,22 @@ spark = SparkSession.builder.appName("Spark DataFrames").getOrCreate()
 
 df = spark.read.option("header",True).csv('/FileStore/tables/StudentData.csv')
 df.show()
+
+# Read option 1
+vehicles_df = spark.read.option("header" , True).option("inferSchema" , True).csv("/Volumes/my_catalog/new_schema/my_volume/sample_data.csv")
+vehicles_df.show()
+
+#Read option 2
+vehicles_df = spark.read.options(header = True, inferSchema = True).csv("/Volumes/my_catalog/new_schema/my_volume/sample_data.csv")
+
+vehicles_df.show()
+
+
+#Read option 3
+
+df = spark.read.format("csv").option("header", True).load("/Volumes/my_catalog/new_schema/my_volume/sample_data.csv")
+
+df.show()
+
+
+vehicles_df.printSchema()
